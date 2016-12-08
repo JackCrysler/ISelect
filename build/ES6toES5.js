28eea5cbe65ebb8411159558365ac7de63aefe42
JackCrysler <jack_crysler@163.com>
2016-12-08 19:42:51 +0800

    dsa

--- build/ES6toES5.js
+++ build/ES6toES5.js
@@ -52,13 +52,15 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	//可以通过回调来获取选择项
 	new _ISelect2.default('.i-select', function (data) {
 	    console.log('您选择的信息是：' + data);
 	});
 
+	//可以通过事件监听来获取选择项
 	var el = document.querySelector('.i-select');
 	el.addEventListener('selected', function (e) {
-	    console.log(e.value);
+	    console.log('\n' + e.value);
 	}, false);
 
 /***/ },
